da45e678b4816367944697fc64bdec27dd01b723
Osvaldo Jeronymo <osvaldo.j.neto@gmail.com>
2025-11-13 13:27:51 -0300

    Fix base URL for silic-input-doc repository

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.', 
-  base: '/show-input-doc/',
+  base: '/silic-input-doc/',
   server: {
     port: 3000,
     open: true
