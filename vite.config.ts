c92740e53c622ade3f8d4582fcff82079b038cce
Osvaldo Jeronymo <osvaldo.j.neto@gmail.com>
2025-12-17 11:25:45 -0300

    Build: corrigir base do Vite e homepage para /silic-input-doc/

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   root: '.', 
   base: '/silic-input-doc/',
   server: {
@@ -12,7 +14,8 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: 'index.html'
+        main: 'index.html',
+        'form-renderer': 'form-renderer.html'
       }
     }
   },
